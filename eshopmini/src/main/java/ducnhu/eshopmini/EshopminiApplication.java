6e8879f1fe6bd97810b2d3987141770ca9c3e949
tobezero01 <ducnhuad@gmail.com>
2025-01-04 20:05:29 +0700

    first commit

---
+++ eshopmini/src/main/java/ducnhu/eshopmini/EshopminiApplication.java
@@ -0,0 +1,13 @@
+package ducnhu.eshopmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopminiApplication.class, args);
+	}
+
+}
---
+++ eshopmini/src/test/java/ducnhu/eshopmini/EshopminiApplicationTests.java
@@ -0,0 +1,13 @@
+package ducnhu.eshopmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
